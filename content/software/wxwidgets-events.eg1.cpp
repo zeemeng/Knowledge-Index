e3c8aa18137436862f45eca8fb2d426c3b2ad92e
Zi Han Meng <zi.han.meng100@gmail.com>
2023-04-25 14:23:20 -0400

    Converted repo into subject agnostic base project
    * Amended "README.md".
    * Removed the license file.
    * Cleared "index.html" of subject specific elements.
    * Removed dir "content/" and its contents.

--- content/software/wxwidgets-events.eg1.cpp
+++
@@ -1,6 +0,0 @@
-wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-  EVT_MENU(wxID_EXIT, MyFrame::OnExit)
-  EVT_MENU(DO_TEST, MyFrame::DoTest)
-  EVT_SIZE(MyFrame::OnSize)
-  EVT_BUTTON(BUTTON1, MyFrame::OnButton1)
-wxEND_EVENT_TABLE()
